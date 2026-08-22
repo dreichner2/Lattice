29bca12365fe514e5bd63560f08e3b3439c3276e
danny <danny@local>
2026-08-21 21:31:13 -0600

    Fix Windows shell framework imports

--- windows/CSLibrary.Windows/MainWindow.xaml.cs
+++ windows/CSLibrary.Windows/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using Microsoft.Win32;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
+using System.Net.Http;
 using System.Text;
 using System.Text.Json;
 using System.Windows;
